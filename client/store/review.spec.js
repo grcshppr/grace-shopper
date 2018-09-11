9e9d1bfe95d286939651465eba0910b9196bc66c
Kevin Gislason <kevingislason@gmail.com>
2018-09-11 16:40:49 -0500

    almost done w/ singlebookview

---
+++ client/store/review.spec.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai'
+
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+import thunkMiddleware from 'redux-thunk'
+import history from '../history'
---
+++ server/api/reviews.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const {Review} = require('../db/models');
+module.exports = router;
+
+router.get('/', async (req, res, next) => {
+  try {
+    const reviews = await Review.findAll();
+    res.json(reviews);
+  } catch(err) {
+    next(err);
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const reviewId = req.params.id;
+    const review = await Review.findById(reviewId);
+    res.json(review);
+  } catch(err) {
+    next(err);
+  }
+})
+
+//Gets all reviews for a specific book
+router.get('/book/:bookId', async (req, res, next) => {
+  try {
+    const bookId = req.params.bookId;
+    const reviewsforBook = await Review.findAll({where: {bookId}});
+    res.json(reviewsforBook);
+  } catch(err) {
+    next(err);
+  }
+})
