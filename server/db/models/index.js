21852e1b898851490a0992df8214be4400602b8c
abby wigdale <abby.wigdale@gmail.com>
2018-09-11 11:27:07 -0500

    finished models with tests, no tests for reviews model- dont think its totally necessary right now. all the tables look good in postico

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,8 @@
 const User = require('./user')
+const Book = require('./books')
+const Review = require('./reviews')
+const Order = require('./orders')
+const OrderBook = require('./orders_books')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +17,27 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+//User-Order
+User.hasMany(Order)
+Order.belongsTo(User)
+
+//User-Review
+User.hasMany(Review)
+Review.belongsTo(User)
+
+//Book-Review
+Book.hasMany(Review)
+Review.belongsTo(Book)
+
+//Book-Order
+Book.belongsToMany(Order, {through: OrderBook})
+Order.belongsToMany(Book, {through: OrderBook})
+
 module.exports = {
-  User
+  User,
+  Book,
+  Review,
+  Order,
+  OrderBook
 }
---
+++ server/db/models/reviews.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Review = db.define("review", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  stars: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+      max: 5
+    }
+  }
+})
+
+module.exports = Review
