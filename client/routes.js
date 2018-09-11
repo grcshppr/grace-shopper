d10b92c6a6f1df4fea28c08f3844623e41dfe455
Kevin Gislason <kevingislason@gmail.com>
2018-09-11 16:41:46 -0500

    nothing much

--- client/routes.js
+++ client/routes.js
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route exact path="/book/:id" component={DetailedBook}
+        <Route exact path="/book/:id" component={DetailedBook} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
