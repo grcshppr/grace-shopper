58ef32af9fd475b748bd144e99bd68629c6d9565
tomasgnr <tomasgnr@gmail.com>
2018-09-18 16:03:02 -0500

    sending email after purchase

--- server/stripe/index.js
+++ server/stripe/index.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const {Order, OrderBook, Cart} = require('../db/models')
+const nodemailer = require('nodemailer')
+
+//SMTP
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'graceshopperbookstack@gmail.com',
+    pass: process.env.SMTP_PASSWORD
+  }
+})
 
 router.post('/', async (req, res) => {
+  const email = req.body.email
+
   try {
     let userId
     if (req.body.userId) {
@@ -10,11 +22,15 @@ router.post('/', async (req, res) => {
     } else {
       userId = null
     }
+
     const newOrder = await Order.create({
       stripeToken: req.body.token,
       userId: userId,
       status: 'created'
     })
+
+    const newOrderId = newOrder.id
+
     const cleanCartArray = req.body.cart.map(cartItem => {
       return {
         bookId: cartItem.id,
@@ -22,14 +38,18 @@ router.post('/', async (req, res) => {
         quantity: cartItem.cartQuantity
       }
     })
+
     await OrderBook.bulkCreate(cleanCartArray, {individualHooks: true})
+
     const {totalPrice} = await Order.findById(newOrder.id)
+
     let {status} = await stripe.charges.create({
       amount: totalPrice,
       currency: 'usd',
       description: `Order Number ${newOrder.id}`,
       source: req.body.token
     })
+
     if (status === 'succeeded') {
       newOrder.set('status', 'processing')
       newOrder.set('paidFor', true)
@@ -41,9 +61,24 @@ router.post('/', async (req, res) => {
           }
         })
       }
+
+      const mailOptions = {
+        from: 'graceshopperbookstack@gmail.com',
+        to: email,
+        subject: 'Thank you for your purchase!',
+        html: `<h2>Thank you for shopping with us!</h2>
+               <h3>Order # ${newOrderId}</h3>
+               <p>We would like to inform you that your payment has been received and Order # ${newOrderId} is being processed. Once the items have been packed, they will be shipped to you immediatly.</p>
+               <p>If you have any questions on your payment, plees feel free to contact us</p>
+               <p>Hope you enjoyed shopping weith us!</p>`
+      }
+
+      transporter.sendMail(mailOptions, function(err, info) {
+        if (err) console.log(err)
+        else console.log(info)
+      })
     }
-    const email = req.body.email
-    const newOrderId = newOrder.id
+
     res.json({status, newOrderId, email})
   } catch (error) {
     console.error(error)
