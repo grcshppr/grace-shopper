315df8296fbe6316784559c0a4607b2df589057a
Kevin Gislason <kevingislason@gmail.com>
2018-09-18 15:32:24 -0500

    Added images on all users page & separated admins from users

--- client/components/admin-page-users.js
+++ client/components/admin-page-users.js
@@ -23,29 +23,35 @@ class AdminUserPage extends Component {
   }
 
   render() {
+    const admins = this.props.users.filter(user => !user.isAdmin)
+    const users = this.props.users.filter(user => user.isAdmin)
+
     return (
       <Container>
+        <h3>Admins</h3>
+        {admins.length &&
+          admins.map(user => {
+            return (
+              <Container>
+                <Header key={user.id} as="h3" attached="top">
+                  <Image size="tiny" src={user.imgUrl} />
+                  {user.email}
+                </Header>
+              </Container>
+            )
+          })}
         <h3>Users</h3>
-        {this.props.isFetching ? (
-          <h1>Fetching</h1>
-        ) : (
-          this.props.users
-            .sort((a, b) => {
-              if (a.isAdmin && !b.isAdmin) return -1
-              else if (b.isAdmin && !a.isAdmin) return 1
-              else return 0
-            })
-            .map(user => {
-              return (
-                <Container>
-                  <Header key={user.id} as="h3" attached="top">
-                    <Image size="tiny" src={user.imgUrl} />
-                    {user.email} {user.isAdmin && ' (ADMIN)'}
-                  </Header>
-                </Container>
-              )
-            })
-        )}
+        {users.length &&
+          users.map(user => {
+            return (
+              <Container>
+                <Header key={user.id} as="h3" attached="top">
+                  <Image size="tiny" src={user.imgUrl} />
+                  {user.email} {user.isAdmin && ' (ADMIN)'}
+                </Header>
+              </Container>
+            )
+          })}
       </Container>
     )
   }
